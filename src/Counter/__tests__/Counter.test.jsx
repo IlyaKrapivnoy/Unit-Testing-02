ec066c672f8b08fc4e9294190868547b0036cc6b
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-02 19:26:41 +0300

    add tests for future component

--- src/Counter/__tests__/Counter.test.jsx
+++ src/Counter/__tests__/Counter.test.jsx
@@ -3,9 +3,37 @@ import Counter from '../Counter';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-test('header renders with correct text', () => {
+it('header renders with correct text', () => {
     render(<Counter />);
     const headerEL = screen.getByTestId('header');
 
     expect(headerEL.textContent).toBe('Counter');
+});
+
+it('counter initially starts with zero', () => {
+    const { getByTestId } = render(<Counter />);
+    const counterEl = getByTestId('counter');
+
+    expect(counterEl.textContent).toBe('0');
+});
+
+it('input contains initial value of 1', () => {
+    const { getByTestId } = render(<Counter />);
+    const inputEl = getByTestId('input');
+
+    expect(inputEl.value).toBe('1');
+});
+
+it('add button renders with +', () => {
+    const { getByTestId } = render(<Counter />);
+    const addBtn = getByTestId('add-btn');
+
+    expect(addBtn.textContent).toBe('+');
+});
+
+it('subtract button renders with -', () => {
+    const { getByTestId } = render(<Counter />);
+    const subtractBtn = getByTestId('subtract-btn');
+
+    expect(subtractBtn.textContent).toBe('-');
 });
