5efa7ecbe527e7537309a59fef316cba9bc2594b
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-04-02 19:35:35 +0300

    add elements described in tests

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Counter.css';
 
 const Counter = () => {
-    return <h3 data-testid='header'>Counter</h3>;
+    const [counterValue, setCounterValue] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
+
+    return (
+        <div>
+            <h3 data-testid='header'>Counter</h3>
+            <h2 data-testid='counter'>{counterValue}</h2>
+            <button data-testid='subtract-btn'>-</button>
+            <input
+                type='number'
+                data-testid='input'
+                value={inputValue}
+                className='text-center'
+            />
+            <button data-testid='add-btn'>+</button>
+        </div>
+    );
 };
 
 export default Counter;
